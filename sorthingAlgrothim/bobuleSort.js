efaf67f781534ce39ac3371891885d1e91d35e83
Mehedi390 <mh0816150@gmail.com>
2023-01-11 06:09:31 -0800

    asd

--- sorthingAlgrothim/bobuleSort.js
+++ sorthingAlgrothim/bobuleSort.js
@@ -2,29 +2,24 @@
 // boubl is a sorthing algrothim . its compare couple of element of this arry and find the largest number
 // if the left number in bigger than right number , left number is swapping to right number . and ints contine
 
-const arr = [
-  ,
-  4512,
-  54,
-  21512,
-  5,
-  12,
-  5,
-  2,
-  15,
-  2,
-  5,
-  5,
-  62,
-  3,
-  6,
-  3,
-  632,
-  65,
-  32,
-  6,
-  23,
-  632,
-  652,
-  3,
-];
+const arr = [4512, 54, 21512, 5, 12, 5];
+const boubleSorth = [];
+const result = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      //   console.log(fristNumber, secondNumber);
+      if (arr[j] > arr[j + 1]) {
+        console.log(arr[j], arr[j + 1]);
+        let tem = arr[j];
+
+        arr[j] = arr[j + 1];
+        console.log("here", arr[j]);
+        arr[j + 1] = tem;
+        console.log("last", arr[j + 1]);
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(result(arr));
